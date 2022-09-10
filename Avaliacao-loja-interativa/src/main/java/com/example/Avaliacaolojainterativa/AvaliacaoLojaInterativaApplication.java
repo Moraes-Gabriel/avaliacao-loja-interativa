200d054ebe03859c353f945e615311948167da96
Moraes-Gabriel <Gabipaiz@gmail.com>
2022-09-10 17:44:02 -0300

    adicionando projeto em angular

--- Avaliacao-loja-interativa/src/main/java/com/example/Avaliacaolojainterativa/AvaliacaoLojaInterativaApplication.java
+++ Avaliacao-loja-interativa/src/main/java/com/example/Avaliacaolojainterativa/AvaliacaoLojaInterativaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AvaliacaoLojaInterativaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AvaliacaoLojaInterativaApplication.class, args);
 	}
